60583548cb1e0f467c8972e378106d8a60b99fa5
Devansh Sahu <sahu.devansh02@gmail.com>
2025-02-07 11:53:35 +0530

    resolved metro.config.js error

--- metro.config.js
+++ metro.config.js
@@ -1,19 +1,32 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+// const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
+// /**
+//  * Metro configuration
+//  * https://reactnative.dev/docs/metro
+//  *
+//  * @type {import('metro-config').MetroConfig}
+//  */
 
-const {
-    wrapWithReanimatedMetroConfig,
-} = require('react-native-reanimated/metro-config');
+// const {
+//     wrapWithReanimatedMetroConfig,
+// } = require('react-native-reanimated/metro-config');
 
 
-const config = {};
+// const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+// module.exports = mergeConfig(getDefaultConfig(__dirname), config);
 
+// module.exports = wrapWithReanimatedMetroConfig(config);
+
+
+
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { wrapWithReanimatedMetroConfig } = require('react-native-reanimated/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+// Merge with Reanimated's Metro configuration
+const config = mergeConfig(defaultConfig, {});
+
+// Wrap with Reanimated Metro Config
 module.exports = wrapWithReanimatedMetroConfig(config);
